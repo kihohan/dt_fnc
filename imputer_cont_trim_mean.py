24c68a55f2ec400adb0c26ba9ece7bb66246ca52
Kiho Han <40457277+kihohan@users.noreply.github.com>
2019-11-29 20:13:45 +0900

    Create imputer_cont_trim_mean.py

---
+++ imputer_cont_trim_mean.py
@@ -0,0 +1,10 @@
+def imputer_cont_trim_mean(data,column_name):
+    from scipy import stats
+    try:
+        data[column_name] = data[column_name].fillna(stats.trim_mean(data[column_name].dropna(), 0.2))
+        data[column_name] = round(data[column_name],2)
+    except:
+        print ('not_changed ->',column_name)
+ ########################
+ for column_name in data:
+    imputer_cont_trim_mean(data,column_name)
