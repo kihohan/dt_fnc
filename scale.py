686b3731d41d0b08f83fd9033c29f43787179e87
Kiho Han <40457277+kihohan@users.noreply.github.com>
2019-11-29 20:16:42 +0900

    Create scale.py

---
+++ scale.py
@@ -0,0 +1,22 @@
+def mm_scale(data,column_name):
+    from sklearn.preprocessing import MinMaxScaler
+    scale = MinMaxScaler().fit_transform(np.array(data[column_name]).reshape(-1, 1)).flatten()
+    data[column_name] = scale
+
+def ma_scale(data,column_name):
+    from sklearn.preprocessing import MaxAbsScaler
+    scale = MaxAbsScaler().fit_transform(np.array(data[column_name]).reshape(-1, 1)).flatten()
+    data[column_name] = scale
+
+def r_scale(data,column_name):
+    from sklearn.preprocessing import RobustScaler
+    scale = RobustScaler().fit_transform(np.array(data[column_name]).reshape(-1, 1)).flatten()
+    data[column_name] = scale
+
+def s_scale(data,column_name):
+    from sklearn.preprocessing import StandardScaler
+    scale = StandardScaler().fit_transform(np.array(data[column_name]).reshape(-1, 1)).flatten()
+    data[column_name] = scale
+#########################
+for column_name in scale_list:
+    mm_scale(data,column_name)
