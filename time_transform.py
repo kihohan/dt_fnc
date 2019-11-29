1f021c5cf7a6eae3cc058df46e7e4a6b9d084d78
Kiho Han <40457277+kihohan@users.noreply.github.com>
2019-11-29 19:53:44 +0900

    Update time_transform.py

--- time_transform.py
+++ time_transform.py
@@ -1,10 +1,9 @@
 def time_transform(data, column_name):
     data[column_name]  = pd.to_datetime(data[column_name])
-    data['year'] = data[colum_name].dt.year 
-    data['month'] = data[colum_name].dt.month
-    data['date'] = data[colum_name].dt.day
-    data['day']  = data[colum_name].dt.weekday
-    data['hour'] = data[colum_name].dt.hour
-    data['minute'] = data[colum_name].dt.minute
-    data['second'] = data[colum_name].dt.second
-  
+    data['year'] = data[column_name].dt.year 
+    data['month'] = data[column_name].dt.month
+    data['dates'] = data[column_name].dt.day
+    data['days']  = data[column_name].dt.weekday
+    data['hour'] = data[column_name].dt.hour
+    data['minute'] = data[column_name].dt.minute
+    data['second'] = data[column_name].dt.second
