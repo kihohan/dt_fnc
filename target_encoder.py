fe0111dd92cf645e89094b5d9417a9a7a6d6ccf0
Kiho Han <40457277+kihohan@users.noreply.github.com>
2019-11-30 17:21:32 +0900

    Update target_encoder.py

--- target_encoder.py
+++ target_encoder.py
@@ -7,15 +7,18 @@ def target_encoder(df, column, target, index=None, method='std'):
     # 인코딩 종류
     if method == 'mean':
         encoded_column = train[column].map(train.iloc[index].groupby(column)[target].mean())
+        dict_ = dict (train.iloc[index].groupby(column)[target].mean())
+        test[column] = test[column].map(dict_)
     elif method == 'median':
         encoded_column = train[column].map(train.iloc[index].groupby(column)[target].median())
+        dict_ = dict (train.iloc[index].groupby(column)[target].median())
+        test[column] = test[column].map(dict_)
     elif method == 'std':
         encoded_column = train[column].map(train.iloc[index].groupby(column)[target].std())
+        dict_ = dict (train.iloc[index].groupby(column)[target].std())
+        test[column] = test[column].map(dict_)
     else:
         raise ValueError("Incorrect method supplied: '{}'. Must be one of 'mean', 'median', 'std'".format(method))
-    
-    dict_ = dict (train.iloc[index].groupby(column)[target].std())
-    test[column] = test[column].map(dict_)
     
     result = pd.concat([encoded_column, test[column]])
     return result
