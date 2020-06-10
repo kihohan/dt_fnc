3cd8f37a36bdb274dec94add15da11bb78a0387d
Kiho Han <40457277+kihohan@users.noreply.github.com>
2020-06-10 11:42:32 +0900

    Update indi_sheet_save.py

--- indi_sheet_save.py
+++ indi_sheet_save.py
@@ -1,4 +1,5 @@
-xlxs_dir = "fil_name.xlsx"
-with pd.ExcelWriter(xlxs_dir) as writer:
-    for brand in result['BRAND'].unique():
-        result[result['BRAND'] == brand].to_excel(writer, sheet_name = brand, index = False)
+def save_sheet_excel(df, excel_name, sheet_column):
+    xlxs_dir = excel_name + ".xlsx"
+    with pd.ExcelWriter(xlxs_dir) as writer:
+        for unique in df[sheet_column].unique():
+            df[df[sheet_column] == unique].to_excel(writer, sheet_name = unique, index = False)
