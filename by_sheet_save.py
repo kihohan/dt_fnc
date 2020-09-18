e36d31b646f74daa705d5d7527c378f63f601e81
Kiho Han <40457277+kihohan@users.noreply.github.com>
2020-09-18 16:23:23 +0900

    Update by_sheet_save.py

--- by_sheet_save.py
+++ by_sheet_save.py
@@ -1,5 +1,5 @@
 def save_sheet_excel(df, excel_name, sheet_column):
     xlxs_dir = excel_name + ".xlsx"
     with pd.ExcelWriter(xlxs_dir) as writer:
-        for unique in df[sheet_column].unique():
+        for unique in sorted(df[sheet_column].unique())
             df[df[sheet_column] == unique].drop(sheet_column,1).to_excel(writer, sheet_name = unique, index = False)
